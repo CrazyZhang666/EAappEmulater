8173e1c8bb9ff9f19ac9421c8ba9f02b6782ed88
穗乃果 <1763575579@qq.com>
2024-05-24 23:46:54 +0800

    update mod tools

--- BF1ModTools/Views/ToolsView.xaml.cs
+++ BF1ModTools/Views/ToolsView.xaml.cs
@@ -64,4 +64,30 @@ public partial class ToolsView : UserControl
     {
         ProcessHelper.OpenDirectory(Globals.BF1InstallDir);
     }
+
+    [RelayCommand]
+    private void FixBF1DocumentJS()
+    {
+        try
+        {
+            if (ProcessHelper.IsAppRun(CoreUtil.Name_BF1))
+            {
+                NotifierHelper.Warning("战地1正在运行，请关闭后再执行修复JS操作");
+                return;
+            }
+
+            var docDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var twinkleDir = Path.Combine(docDir, "Battlefield 1\\twinkle");
+            FileHelper.CreateDirectory(twinkleDir);
+
+            FileHelper.ExtractResFile("Exec.bundle-5388698.js", Path.Combine(twinkleDir, "bundle-5388698.js"));
+            FileHelper.ExtractResFile("Exec.manifest-5388698", Path.Combine(twinkleDir, "manifest-5388698"));
+
+            NotifierHelper.Success("执行修复战地1文档JS操作成功");
+        }
+        catch (Exception ex)
+        {
+            LoggerHelper.Error($"修复战地1文档JS发生异常", ex);
+        }
+    }
 }
