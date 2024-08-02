16c4a962ab5447d3dd51541f01cee980f4c2648d
穗乃果 <1763575579@qq.com>
2024-08-02 17:38:08 +0800

    发布新版本 v1.7.0.0

--- EAappEmulater/Properties/AssemblyInfo.cs
+++ EAappEmulater/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@
 [assembly: AssemblyTitle("EA app 模拟器")]
 [assembly: AssemblyCompany("https://github.com/CrazyZhang666/EAappEmulater")]
 [assembly: AssemblyProduct("EA app 模拟器")]
-[assembly: AssemblyVersion("1.6.2.0")]
-[assembly: AssemblyFileVersion("1.6.2.0")]
-[assembly: AssemblyInformationalVersion("1.6.2.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.7.0.0")]
 
 // 不重复
 [assembly: AssemblyDescription("https://github.com/CrazyZhang666/EAappEmulater")]
